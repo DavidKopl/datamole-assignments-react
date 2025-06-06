e3c5408681dc3eb95f1fd96ecae21eaf52abdb1d
David Kopl - PC <kopldavid@seznam.cz>
2025-06-06 18:19:57 +0200

    UI3

--- client/src/components/ListItem.tsx
+++ client/src/components/ListItem.tsx
@@ -1,12 +1,30 @@
-import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { Checkbox } from "./Checkbox";
 import { Button } from "./Button";
+import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
 
-const StyledDiv = styled.div`
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const ListItemWrapper = styled.div`
     display: flex;
     align-items: center;
+    padding: 8px;
+
+    &:hover > div.actions {
+        visibility: visible;
+        opacity: 1;
+        animation: ${fadeIn} 0.3s ease forwards;
+    }
 `;
 
 const Label = styled.label`
@@ -14,12 +32,15 @@ const Label = styled.label`
     margin-left: 15px;
 `;
 
-const Actions = styled.div`
+const Actions = styled.div.attrs({ className: "actions" })`
     display: flex;
-    gap: 8px; // mezera mezi tlačítky
-    margin-left: auto; // tlačítka posune doprava
-`;
+    gap: 8px;
+    margin-left: auto;
 
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 0.3s ease;
+`;
 export type LiteeItemProp = {
     label: string;
     isDone: boolean;
@@ -27,26 +48,25 @@ export type LiteeItemProp = {
     onItemDoneToggle: (isDone: boolean) => void;
     onItemDelete: () => void;
 };
-
 export const ListItem = (props: LiteeItemProp) => {
     const { label, isDone, onItemLabelEdit, onItemDoneToggle, onItemDelete } = props;
 
     const [isEditing, setIsEditing] = useState(false);
     const [editedLabel, setEditedLabel] = useState(label);
 
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         onItemLabelEdit(editedLabel);
         setIsEditing(false);
     };
 
     return (
-        <StyledDiv>
+        <ListItemWrapper>
             <Checkbox checked={isDone} onCheckedChange={onItemDoneToggle} />
 
             {isEditing ? (
                 <form onSubmit={handleSubmit}>
-                    <input type="text" value={editedLabel} onChange={(e) => setEditedLabel(e.target.value)} />
+                    <input type="text" value={editedLabel} onChange={(e) => setEditedLabel(e.target.value)} autoFocus />
                     <Button type="submit">Save</Button>
                     <Button type="button" onClick={() => setIsEditing(false)}>
                         Cancel
@@ -65,6 +85,6 @@ export const ListItem = (props: LiteeItemProp) => {
                     </Actions>
                 </>
             )}
-        </StyledDiv>
+        </ListItemWrapper>
     );
 };
